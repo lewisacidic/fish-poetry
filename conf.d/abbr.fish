4c03ca2891f7433e9720d1cd6936ff9008b8f54f
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2020-02-01 17:06:16 +0000

    feat: add abbrs

---
+++ conf.d/abbr.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+#
+# Copyright (c) 2020 Rich Lewis
+# License: MIT
+
+abbr p 'poetry'
+abbr pon 'poetry new --src'
+abbr poin 'poetry init'
+abbr poi 'poetry install'
+abbr poup 'poetry update'
+abbr poa 'poetry add'
+abbr porm 'poetry remove'
+abbr pos 'poetry show'
+abbr pob 'poetry build'
+abbr pop 'poetry publish'
+abbr poc 'poetry config'
+abbr por 'poetry run'
+abbr posh 'poetry shell'
+abbr pock 'poetry check'
+abbr pof 'poetry search'
+abbr pol 'poetry lock'
+abbr pov 'poetry version'
+abbr pox 'poetry export'
+abbr poe 'poetry env'
+
